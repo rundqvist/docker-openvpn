4f5b6ffdcb4c94956f960bde53a6bffd795526b7
Mattias Rundqvist <mattias.rundqvist@icloud.com>
2020-09-02 18:52:09 +0200

    Update healthcheck.sh

--- root/app/openvpn/healthcheck.sh
+++ root/app/openvpn/healthcheck.sh
@@ -1,16 +1,10 @@
 #!/bin/sh
 
-if [ -z "$(getv VPN_PROVIDER)" ]; then
+if [ -z "$(var VPN_PROVIDER)" ]; then
 
     echo "No VPN provider specified."
     exit 1;
 
-fi
-
-if [ "$(getv VPN_MULTIPLE)" = "true" ]; then
-
-    echo "Multiple VPN configured."
-    exit 0;
 fi
 
 VPNIP=$(wget http://api.ipify.org -O - -q 2>/dev/null)
@@ -20,6 +14,8 @@ IP=$(cat /app/openvpn/ip)
 if [ $RC -eq 1 ]; then
     echo "No internet connection."
     exit 1;
+elif [ "$(var VPN_MULTIPLE)" = "true" ]; then
+    echo "Multiple VPN. "
 elif [[ ${IP:0:1} = "1" ]]; then
     echo "IP could not be resolved before connecting to VPN. Privacy could be compromized. Public IP is: $VPNIP."
     exit 1;
@@ -28,6 +24,6 @@ elif [ $RC":"$VPNIP = $IP ]; then
 	exit 1;
 fi
 
-echo "Public IP is: $VPNIP. ";
+echo "Public IP: $VPNIP. ";
 
 exit 0;
