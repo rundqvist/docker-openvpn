e14d75260de7235350c60a5dfcef85660d658f6e
Mattias Rundqvist <mattias.rundqvist@icloud.com>
2020-09-17 19:55:27 +0200

    Changed mandatory to script

---
+++ root/app/mandatory.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+for var in "VPN_PROVIDER" "VPN_USERNAME" "VPN_PASSWORD" "VPN_COUNTRY" ; do 
+
+    if [ -z "$(var $var)" ] ; then
+        log -e openvpn "Environment variable '$var' is mandatory. "
+        var abort true
+    else
+        log -d openvpn "Mandatory variable '$var' is ok."
+    fi
+
+done
+
+if [ "$(var abort)" = "true" ] ; then
+    exit 1;
+fi
+exit 0;
