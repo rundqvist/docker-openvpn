e481d6d42a1b15a121edab47df165519de890ede
Mattias Rundqvist <mattias.rundqvist@icloud.com>
2020-09-20 10:04:40 +0200

    Update healthcheck.sh

--- root/app/openvpn/healthcheck.sh
+++ root/app/openvpn/healthcheck.sh
@@ -1,17 +1,23 @@
 #!/bin/sh
 
+if [ "$(var VPN_MULTIPLE)" = "true" ]
+then
+    log -v openvpn "[health] Health check (Multiple VPN)."
+    echo "Multiple VPN. "
+    exit 0;
+fi
+
 log -v openvpn "[health] Check health"
 
 VPNIP=$(wget http://api.ipify.org -T 10 -O - -q 2>/dev/null)
 
-if [ $? -eq 1 ]; then
+if [ $? -eq 1 ]
+then
     var fail + 1
     log -e openvpn "[health] No internet connection ($(var fail))."
     echo "No internet connection ($(var fail))."
-elif [ "$(var VPN_MULTIPLE)" = "true" ]; then
-    var -v fail
-    echo "Multiple VPN. "
-elif [ "$(var publicIp)" = "$VPNIP" ]; then
+elif [ "$(var publicIp)" = "$VPNIP" ]
+then
     var fail + 1
     log -e openvpn "[health] Not connected to VPN. Public IP is: $VPNIP ($(var fail)).";
 	echo "Not connected to VPN. Public IP is: $VPNIP ($(var fail)).";
