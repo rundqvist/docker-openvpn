5e6609162506944593c162180e9bf0f95f2c4fdf
Mattias Rundqvist <mattias.rundqvist@icloud.com>
2020-09-20 10:22:53 +0200

    Update healthcheck.sh

--- root/app/openvpn/healthcheck.sh
+++ root/app/openvpn/healthcheck.sh
@@ -13,30 +13,30 @@ VPNIP=$(wget http://api.ipify.org -T 10 -O - -q 2>/dev/null)
 
 if [ $? -eq 1 ]
 then
-    var fail + 1
-    log -e openvpn "[health] No internet connection ($(var fail))."
-    echo "No internet connection ($(var fail))."
+    var openvpn.fail + 1
+    log -e openvpn "[health] No internet connection ($(var openvpn.fail))."
+    echo "No internet connection ($(var openvpn.fail))."
 elif [ "$(var publicIp)" = "$VPNIP" ]
 then
-    var fail + 1
-    log -e openvpn "[health] Not connected to VPN. Public IP is: $VPNIP ($(var fail)).";
-	echo "Not connected to VPN. Public IP is: $VPNIP ($(var fail)).";
+    var openvpn.fail + 1
+    log -e openvpn "[health] Not connected to VPN. Public IP is: $VPNIP ($(var openvpn.fail)).";
+	echo "Not connected to VPN. Public IP is: $VPNIP ($(var openvpn.fail)).";
 else
-    var -d fail
+    var -d openvpn.fail
     log -v openvpn "[health] VPN IP is: $VPNIP."
     echo "VPN IP: $VPNIP. ";
 fi
 
-if [ "$(var fail)" = "3" ]
+if [ "$(var openvpn.fail)" = "3" ]
 then
-    var -d fail
+    var -d openvpn.fail
     country=$(var VPN_COUNTRY)
     log -i openvpn "[health] Restarting VPN."
     pid=$(ps -o pid,args | sed -n "/openvpn\/config-$country/p" | awk '{print $1}')
 
     kill -s SIGHUP $pid
 
-elif [ -z "$(var fail)" ]; then
+elif [ -z "$(var openvpn.fail)" ]; then
     exit 0;
 fi
 
